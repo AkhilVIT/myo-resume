aa808a418a265adfe20c81ea6fbbebcdafceabf3
AkhilVIT <makhilakhi01@gmail.com>
2020-08-18 01:35:01 +0530

    Updates

---
+++ precache-manifest.0c32a212d5a32aa7e2244f38e73fcd5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4c9189640d35211b589fae28eac5504",
+    "url": "/myo-resume/index.html"
+  },
+  {
+    "revision": "d0062369cf874a18b23d",
+    "url": "/myo-resume/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "822279db194cc7199500",
+    "url": "/myo-resume/static/js/2.992caa54.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/myo-resume/static/js/2.992caa54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0062369cf874a18b23d",
+    "url": "/myo-resume/static/js/main.73030ed2.chunk.js"
+  },
+  {
+    "revision": "0ceaaf3858bc0ec0f5f4",
+    "url": "/myo-resume/static/js/runtime-main.1b154e68.js"
+  },
+  {
+    "revision": "a47c24e00058669826f7380dbb59dc7a",
+    "url": "/myo-resume/static/media/myo-logo1.a47c24e0.svg"
+  }
+]);
