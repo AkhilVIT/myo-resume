26ce7e8a5e3dbd7387bba989a0e77bbb244b32a6
AkhilVIT <makhilakhi01@gmail.com>
2020-09-06 23:38:26 +0530

    make it better

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import './App.css';
-
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 function App() {
   return (
     <div className="App">
-       <Navbar/>
+      <h1>Welcome to MYO Resume</h1>
+      <MessengerCustomerChat
+        pageId="108138601028407"
+        appId="304838707479650"
+        //htmlRef="<REF_STRING>"
+      />
     </div>
   );
 }
